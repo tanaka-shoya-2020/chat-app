48c78d1a3ef5952427b7c22671da27b9d2cb3e51
tanaka-shoya-2020 <71364105+tanaka-shoya-2020@users.noreply.github.com>
2020-10-20 16:03:44 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,10 +9,13 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
+      # binding.pry
+      # render :index
       redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
-      render :index
+      # render :index
+      redirect_to room_messages_path
     end
   end
 
